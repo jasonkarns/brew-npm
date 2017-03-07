e4abd38e0e8f9e7243e5c7b68a56a25815cc4de2
Jason Karns <jason@karns.name>
2017-03-07 11:15:25 -0500

    module movement

--- lib/brew/npm/cli.rb
+++ lib/brew/npm/cli.rb
@@ -2,43 +2,30 @@ require 'brew/npm/commands'
 
 module Brew
   module Npm
-    class CLI
-      def initialize(args = ARGV)
-        @command_name, *@args = args
-
-        abort command_required unless @command_name
+    class CommandRequired < StandardError
+      def initialize(msg="Please specify a command")
+        super
       end
+    end
 
-      def run
-        command.call
-      end
+  end
+end
 
-      private
+module Brew
+  module Npm
+    module CLI
+      def self.run(*args)
+        command_name = args.pop
 
-      def command
-        @command ||= command_class.new @args
-      end
+        raise CommandRequired unless command_name
 
-      def command_class
-        classname = @command_name.to_s.capitalize
-        abort unknown_command(@command_name) unless Commands.const_defined? classname
-
-        Commands.const_get(classname, false)
-      end
-
-      def command_required
-        "Please specify a command"
-      end
-
-      def unknown_command(command)
-        "Unknown command: #{command}"
+        Commands.get(command_name).new(args).call
       end
 
       # def help_msg
       #   (["Please specify a gem name (e.g. brew gem command <name>)"] +
       #    COMMANDS.map {|name, desc| "  #{name} - #{desc}"}).join("\n")
       # end
-
     end
   end
 end
--- lib/brew/npm/commands.rb
+++ lib/brew/npm/commands.rb
@@ -3,75 +3,87 @@ require 'brew/npm/formula'
 
 module Brew
   module Npm
-    class CLI
-      class Command
-        def name
-          self.class.name.split('::').last.downcase
+    class UnknownCommand < StandardError
+      def initialize(msg="Unknown command", cmd)
+        super("#{msg}: #{cmd}")
+      end
+    end
+
+    class Command
+      def name
+        self.class.name.split('::').last.downcase
+      end
+    end
+
+    #TODO accept homebre aliases (like remove for uninstall)
+    #TODO accept addition homebrew options
+    #TODO only Install/Upgrade needs the version arg
+    #TODO completions?
+    class BrewCommand < Command
+      def initialize(args=[])
+        name, @options = process_args(args)
+        @formula = Formula.new Package.new(name.pop)
+      end
+
+      def call
+        @formula.write_temporarily do |filename|
+          system "brew", name, filename, *@options
         end
       end
 
-      #TODO accept homebre aliases (like remove for uninstall)
-      #TODO accept addition homebrew options
-      #TODO only Install/Upgrade needs the version arg
-      #TODO completions?
-      class BrewCommand < Command
-        def initialize(args=[])
-          name, @options = process_args(args)
-          @formula = Formula.new Package.new(name.pop)
-        end
+      private
+
+      def process_args(args)
+        args.partition { |arg| !arg.start_with? '--' }
+      end
+    end
+
+    module Commands
+      def self.get(name)
+        classname = name.to_s.capitalize
+
+        raise UnknownCommand, name unless const_defined? classname
+
+        const_get(classname, false)
+      end
+
+      class Help < Command
+        @@help = "This message"
 
         def call
-          @formula.write_temporarily do |filename|
-            system "brew", name, filename, *@options
-          end
-        end
-
-        private
-
-        def process_args(args)
-          args.partition { |arg| !arg.start_with? '--' }
+          puts "HELPY"
         end
       end
 
-      module Commands
-        class Help < Command
-          @@help = "This message"
-
-          def call
-            puts "HELPY"
-          end
-        end
-
-        class Install < BrewCommand
-          @@help = <<-MSG
+      class Install < BrewCommand
+        @@help = <<-MSG
 Install a brew gem, accepts an optional version argument
             (e.g. brew gem install <name> [version])
-          MSG
-        end
+        MSG
+      end
 
-        class Upgrade < BrewCommand
-          @@help = "Upgrade outdated <npm package>s."
-        end
+      class Upgrade < BrewCommand
+        @@help = "Upgrade outdated <npm package>s."
+      end
 
-        class Uninstall < BrewCommand
-          @@help = "Uninstall <npm package>."
-        end
+      class Uninstall < BrewCommand
+        @@help = "Uninstall <npm package>."
+      end
 
-        class Home < BrewCommand
-          @@help = "Open <npm package>'s homepage in a browser."
-        end
+      class Home < BrewCommand
+        @@help = "Open <npm package>'s homepage in a browser."
+      end
 
-        class Info < BrewCommand
-          @@help = "Display information about <npm package>."
-        end
+      class Info < BrewCommand
+        @@help = "Display information about <npm package>."
+      end
 
-        class Options < BrewCommand
-          @@help = "Display install options specific to <npm package>."
-        end
+      class Options < BrewCommand
+        @@help = "Display install options specific to <npm package>."
+      end
 
-        class List < BrewCommand
-          @@help = "List the installed files for <npm package>."
-        end
+      class List < BrewCommand
+        @@help = "List the installed files for <npm package>."
       end
     end
   end
