f591623ae5c8062c1ac7ac13fb08769d0eff2c79
Jason Karns <jason@karns.name>
2017-03-06 19:30:41 -0500

    cleanup

--- lib/brew/npm/package.rb
+++ lib/brew/npm/package.rb
@@ -1,6 +1,19 @@
 module Brew
   module Npm
     class Package < Struct.new(:name, :version)
+      #TODO: verify package exists on npm
+      #TODO: get version if not provided
     end
   end
 end
+
+    # def fetch_version(name, version = nil)
+    #   gems = `gem list --remote "^#{name}$"`.lines
+
+    #   unless gems.detect { |f| f =~ /^#{name} \(([^\s,]+).*\)/ }
+    #     abort "Could not find a valid gem '#{name}'"
+    #   end
+
+    #   version ||= $1
+    #   version
+    # end
