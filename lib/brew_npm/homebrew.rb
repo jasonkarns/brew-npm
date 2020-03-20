35e185ee64843d6ed3313ad7c40acd6e95d8343e
Jason Karns <jason.karns@gmail.com>
2020-03-20 17:10:44 -0400

    wip1

---
+++ lib/brew_npm/homebrew.rb
@@ -0,0 +1,35 @@
+module BrewNpm
+  class Homebrew
+    def install
+
+    end
+
+    def uninstall
+
+    end
+
+    def info
+
+    end
+
+    def home
+
+    end
+
+    def options
+
+    end
+
+    def upgrade
+
+    end
+
+    def list
+
+    end
+
+    def edit
+
+    end
+  end
+end
